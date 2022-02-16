e2079f7839fc98dee60264781dcb45889eddc0d8
danydf1 <reitmatch@hotmail.com>
2022-02-15 23:46:34 -0300

    code

---
+++ scraping.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
+#https://www.youtube.com/watch?v=y7-Zm5wdARU jony devrock
+
+url = "https://www.lanacion.com.ar"
+page= requests.get(url)
+
+soup = BeautifulSoup(page.content,"html.parser")
+
+titulos = soup.find_all('h2', "com-title")
+
+for til in titulos:
+    sub = til.find_all('a')
+    print(sub[0].attrs.get('title'))
+
+
